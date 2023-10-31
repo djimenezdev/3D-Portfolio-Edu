630cb1d56032e3aa2af84fae241fa932bb61a81c
Daniel Jimenez <daniel@djimenezdev.com>
2023-10-31 03:44:26 -0700

    react router dom implementation

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,27 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Music from "./components/Music/Music.jsx";
+import Credits from "./components/Credits/Credits.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/halloweenMusic",
+    element: <Music />,
+  },
+  {
+    path: "credits",
+    element: <Credits />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <>
-      <App />
-    </>
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
